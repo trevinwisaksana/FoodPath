c70b1fdd75f030f8376bc9b6daa68c009663ffb4
Trevin Wisaksana <trevinwisaksana@gmail.com>
2017-04-17 11:49:23 -0700

    • Version update

---
+++ ProductMap/SearchView.swift
@@ -0,0 +1,24 @@
+//
+//  SearchView.swift
+//  ProductMap
+//
+//  Created by Trevin Wisaksana on 4/16/17.
+//  Copyright © 2017 Trevin Wisaksana. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Miscellaneous setup
+        backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
