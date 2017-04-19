ff51860ce8616181c2d2c8011f8ef65b5e494c41
Trevin Wisaksana <trevinwisaksana@gmail.com>
2017-04-19 07:31:23 -0700

    • Version update

--- ProductMap/APIClient.swift
+++ ProductMap/APIClient.swift
@@ -86,6 +86,9 @@ class APIClient {
         })
     }
     
+    
+    
+    
 
 }
 
--- ProductMap/MainViewController.swift
+++ ProductMap/MainViewController.swift
@@ -16,6 +16,7 @@ class MainViewController: UIViewController, SearchProductDelegate {
     var listOfCategories: [Category] = []
     // List of Products
     var listOfProducts: [Product] = []
+    var listOfFilteredProducts: [Product] = []
     
     // MARK: - Views
     let mainMapView = MainMapView()
