2f00fa0aa161bf226f16b416c2b4ccdd12744b49
Trevin Wisaksana <trevinwisaksana@gmail.com>
2017-05-15 11:06:51 +0700

    • Updated title and color of the addFoodButton

--- ProductMap/AddFoodButton.swift
+++ ProductMap/AddFoodButton.swift
@@ -22,30 +22,33 @@ class AddFoodButton: UIButton {
     
     /// Setup for add food button
     public func setupAddFoodButton(superview: UIView) {
-        self.mainView = superview
+        mainView = superview
         
         let width = superview.frame.width * 0.2
         
-        self.setTitle("Add food location", for: .normal)
-        self.backgroundColor = .white
-        self.layer.shadowOffset = CGSize(
-            width: 0,
-            height: 0
-        )
-        self.layer.shadowOpacity = 0.2
-        self.frame = CGRect(
+        frame = CGRect(
             x: superview.frame.width * 0.73,
             y: superview.frame.height * 0.85,
             width: width,
             height: width
         )
-        self.layer.cornerRadius = self.frame.width / 2
+        
+        // Attributes setup
+        setTitle("+", for: .normal)
+        titleLabel?.font = UIFont(name: "Averta", size: 30)
+        backgroundColor = .blue
+        layer.shadowOffset = CGSize(
+            width: 0,
+            height: 0
+        )
+        layer.shadowOpacity = 0.2
+        layer.cornerRadius = frame.width / 2
         
         let gestureRecognizer = UITapGestureRecognizer(
             target: self,
             action: #selector(addFoodButtonHandler)
         )
-        self.addGestureRecognizer(gestureRecognizer)
+        addGestureRecognizer(gestureRecognizer)
         
         delegate = superview as? AddFoodButtonDelegate
         
@@ -56,6 +59,7 @@ class AddFoodButton: UIButton {
     @objc fileprivate func addFoodButtonHandler() {
         
         let width = mainView.frame.width * 0.2
+        
         // Check if the crosshair is hidden or not
         if crosshairIsHidden == true {
             crosshairIsHidden = false
@@ -68,6 +72,8 @@ class AddFoodButton: UIButton {
             
             delegate?.displayCrosshair()
             
+            // self.titleLabel?.isHidden = true
+        
             UIView.animate(withDuration: 0.2, animations: {
                 
                 self.frame = CGRect(
@@ -76,15 +82,20 @@ class AddFoodButton: UIButton {
                     width: -self.frame.width * 4.27,
                     height: width
                 )
-                self.backgroundColor = .blue
                 
+            }, completion: { (_) in
+                // Change title
+                self.setTitle("Add food location", for: .normal)
+                self.titleLabel?.font = UIFont(name: "Averta", size: 30)
             })
             
         } else {
+            
             // Sets to true to have other portion of code to run
             crosshairIsHidden = true
             // Add pin
             delegate?.addPin()
+            
         }
         
     }
@@ -109,7 +120,9 @@ class AddFoodButton: UIButton {
                 height: width
             )
             
-            self.backgroundColor = .white
+            // Change title
+            self.setTitle("+", for: .normal)
+            self.titleLabel?.font = UIFont(name: "Averta", size: 30)
             
         })
         
